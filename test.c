86770665905398162bd71b65b6764da1c412e604
mfortuna <mariafortunatom@gmail.com>
2023-12-06 15:26:21 +0000

    completed

--- test.c
+++ test.c
@@ -1,24 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfortuna <mfortuna@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/09 15:40:50 by mfortuna          #+#    #+#             */
-/*   Updated: 2023/11/14 12:06:47 by mfortuna         ###   ########.fr       */
+/*   Created: 2023/12/06 11:30:18 by mfortuna          #+#    #+#             */
+/*   Updated: 2023/12/06 11:35:29 by mfortuna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdarg.h>
+#include "mylibft.h"
 
-int	ft_printf(const char *s, ...);
-int	ft_printchar(char c);
-int	ft_printstr(const char *s);
-int	ft_nbr(const char *s, va_list args);
+int	main(void)
+{
+	char *str;
+	int fd = open("./testall.txt", O_RDONLY);
+	int nbr;
 
-#endif
+	str = get_next_line(fd);
+	ft_printf("%s", str);
+	close(fd);
+	nbr = ft_atoi(str);
+	ft_printf("..%i \n", nbr);
+}
